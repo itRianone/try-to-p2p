854f368b48414f02ff76fb296b759756a8c9ffa8
itRianone <timokhindimitry@gmail.com>
2022-06-28 00:32:29 +0300

    Revert "add orders & their checking handlers"
    This reverts commit e1c35a977cb343f9c1cd560cb7dde0a318693cf5.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio
+from enum import auto
 import logging
 
 from markup import buy_menu
@@ -6,6 +7,7 @@ from markup import buy_menu
 from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.dispatcher import Dispatcher
+from aiogram.types.message import ContentType
 
 from dotenv import dotenv_values
 
@@ -13,7 +15,7 @@ from pyqiwip2p import QiwiP2P
 
 config = dotenv_values(".env")
 
-amount = 1 # cost of item
+amount = 5 # cost of item
 lifetime = 10 # mins bill will be living 
 comment = 'hi' # comment in bill
 
@@ -30,7 +32,7 @@ dp = Dispatcher(bot, loop=loop)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     bill = p2p.bill(amount=amount, lifetime=lifetime, comment=comment)
-    
+
     #alt_url redirect to pay url
     await bot.send_message(message.from_user.id, f'payment_url: {bill.alt_url}', 
                             parse_mode='html',
@@ -47,4 +49,3 @@ async def check(callback: types.CallbackQuery):
 
 if __name__ == '__main__':
     executor.start_polling(dp, loop=loop)
-
